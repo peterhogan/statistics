7aabe9a5069cf5e4db0dd5eacb977465898528d6
Peter Hogan <peterjameshogan@gmail.com>
2017-06-18 21:26:45 +0100

    need to fit dists

--- mention_times.py
+++ mention_times.py
@@ -5,6 +5,9 @@ from time import sleep
 from random import betavariate
 from random import normalvariate
 from datetime import datetime
+from scipy.stats import norm,gamma
+from numpy import linspace
+from pylab import plot,show,hist,figure,title
 
 class Entity(object):
 
@@ -27,20 +30,45 @@ class Entity(object):
             try:
                 time2 = datetime.strptime(self.cache[i+1],"%Y-%m-%d %H:%M:%S") 
                 diff = time2-time1
-                self.diffs.append(diff)
+                self.diffs.append(diff.total_seconds())
             except IndexError:
                 pass
         return self.diffs
+
+    def x(self):
+        x = linspace(0,max(self.diffs),100)
+        return x
+
+    def fit_params(self):
+        param = gamma.fit(self.diffs)
+        return param
+
+    def fit(self):
+        x = self.x()
+        param = self.fit_params()
+        pdf_fitted = gamma.pdf(x, param[0])
+        return pdf_fitted
+        #return [pdf_fitted,param,x]
 
     def __str__(self):
         return self.name+": "+str(self.cache)
 
 t1 = Entity("Hello")
 
-while len(t1.cache) < 25:
+while len(t1.cache) < 30:
     t1.mention()
-    sleep(abs(normalvariate(5,5)))
+    sleep(gamma.rvs(1))
 
+print(t1.diffs)
+
+'''
 print(t1.differences())
 for t in t1.differences():
-    print(t.total_seconds())
+    print(t)
+t1fit = t1.fit()
+print(t1.fit_params())
+
+plot(t1.x(), t1fit,'-r')
+hist(t1.diffs)
+show()
+'''
